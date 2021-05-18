24bcab1dadf0389f19c0beb2bb54297c94cb77ab
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-05-18 21:04:02 +0200

    x

--- imagenet/pt_train_simple.py
+++ imagenet/pt_train_simple.py
@@ -272,8 +272,8 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         # measure data loading time
         data_time.update(time.time() - end)
 
-        images = images.to(device, non_blocking=True)
-        target = target.to(device, non_blocking=True)
+        images = images.to(device)
+        target = target.to(device)
 
         # compute output
         output = model(images)
