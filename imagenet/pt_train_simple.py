c4d70fbfb2ae1da99559ed341b83afe00a032e26
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-05-18 21:02:52 +0200

    x

--- imagenet/pt_train_simple.py
+++ imagenet/pt_train_simple.py
@@ -267,6 +267,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
 
     end = time.time()
     for i, (images, target) in enumerate(train_loader):
+        torch.distributed.barrier()
         print(i)
         # measure data loading time
         data_time.update(time.time() - end)
