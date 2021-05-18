c82a2abd6c40d4b03d07925fdfb1f1d05bacf986
Adrian Wälchli <aedu.waelchli@gmail.com>
2021-05-18 22:03:23 +0200

    refactor

--- imagenet/pytorch/train.py
+++ imagenet/pytorch/train.py
@@ -28,7 +28,6 @@ python -m torch.distributed.launch --nnodes 1  --nproc_per_node 2 --master_addr 
 import argparse
 import os
 import random
-import shutil
 import time
 
 import torch
@@ -42,8 +41,17 @@ import torch.utils.data.distributed
 import torchvision.datasets as datasets
 import torchvision.models as models
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import DataLoader
 from torch.utils.data.distributed import DistributedSampler
+
+from imagenet.pytorch.utils import (
+    AverageMeter,
+    FakeImageNetDataset,
+    ProgressMeter,
+    accuracy,
+    adjust_learning_rate,
+    save_checkpoint,
+)
 
 parser = argparse.ArgumentParser(description="PyTorch ImageNet Training")
 parser.add_argument("--data", default=".", metavar="DIR", help="path to dataset")
@@ -334,88 +342,6 @@ def validate(val_loader, model, criterion, device, args):
         print(f" * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}")
 
     return top1.avg
-
-
-def save_checkpoint(state, is_best, filename="checkpoint.pth.tar"):
-    torch.save(state, filename)
-    if is_best:
-        shutil.copyfile(filename, "model_best.pth.tar")
-
-
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-
-    def __init__(self, name, fmt=":f"):
-        self.name = name
-        self.fmt = fmt
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-    def __str__(self):
-        fmtstr = "{name} {val" + self.fmt + "} ({avg" + self.fmt + "})"
-        return fmtstr.format(**self.__dict__)
-
-
-class ProgressMeter(object):
-    def __init__(self, num_batches, meters, prefix=""):
-        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
-        self.meters = meters
-        self.prefix = prefix
-
-    def display(self, batch):
-        entries = [self.prefix + self.batch_fmtstr.format(batch)]
-        entries += [str(meter) for meter in self.meters]
-        print("\t".join(entries))
-
-    def _get_batch_fmtstr(self, num_batches):
-        num_digits = len(str(num_batches // 1))
-        fmt = "{:" + str(num_digits) + "d}"
-        return "[" + fmt + "/" + fmt.format(num_batches) + "]"
-
-
-class FakeImageNetDataset(Dataset):
-    def __len__(self):
-        return int(1e6)
-
-    def __getitem__(self, item):
-        img = torch.rand(3, 224, 224)
-        label = random.randint(0, 999)
-        return img, label
-
-
-def adjust_learning_rate(optimizer, epoch, args):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = args.lr * (0.1 ** (epoch // 30))
-    for param_group in optimizer.param_groups:
-        param_group["lr"] = lr
-
-
-def accuracy(output, target, topk=(1,)):
-    """Computes the accuracy over the k top predictions for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
-
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-        res = []
-        for k in topk:
-            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
 
 
 if __name__ == "__main__":
